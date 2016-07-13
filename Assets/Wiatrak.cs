4c1609c1410919d442a882f7a38ff2ed29b2b257
Michal <kroko440@wp.pl>
2016-07-13 21:07:21 +0200

    Obracajacy sie wiatrak na domku

---
+++ Assets/Wiatrak.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wiatrak : MonoBehaviour {
+
+	[SerializeField]int jednorazowyObrot = 1;
+	[SerializeField]float czasMiedzyObrotami = 0.3f;
+	float czasOstatniegoObrotu = 0.0f;
+	Transform glowica = null;
+
+	void Awake () {
+		glowica = transform.FindChild ("Glowica");
+	}
+
+	void Update () {
+		if (Time.timeSinceLevelLoad > czasOstatniegoObrotu + czasMiedzyObrotami) {
+			glowica.RotateAround (glowica.position, Vector3.right, jednorazowyObrot);
+			czasOstatniegoObrotu = Time.timeSinceLevelLoad;
+		}
+	}
+}
