84fc2536cd06a03b83bc4c82dc9bbd904d9a40e0
Mirek <miroslawb@o2.pl>
2016-07-13 15:42:26 +0200

    Dodałem drzwi do projektu i ich grafikę

---
+++ Assets/Skrypty/Domek1/OtwieranieDrzwi.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OtwieranieDrzwi : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Physics.queriesHitTriggers = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseDown(){
+		Debug.Log ("pukamy");
+	}
+
+
+
+}
