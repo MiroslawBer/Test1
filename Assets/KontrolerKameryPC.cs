570ac313760e60209a7c0e3506c99d519a69194e
Michal <kroko440@wp.pl>
2016-07-17 14:31:42 +0200

    Prefab kamery, skrypty testowe do obslugi dotyku i ustawiania jednostek na wskazanym polu

---
+++ Assets/KontrolerKameryPC.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KontrolerKameryPC : MonoBehaviour {
+
+	[SerializeField] float predkosc = 0.2F;
+	[SerializeField] float predkoscZoom = 20f;
+
+	void Update() {
+		if (Input.GetKey(KeyCode.UpArrow))
+			transform.Translate(Vector3.forward * predkosc);
+		if (Input.GetKey(KeyCode.DownArrow))
+			transform.Translate(Vector3.back * predkosc);
+		if (Input.GetKey(KeyCode.LeftArrow))
+			transform.Translate(Vector3.left * predkosc);
+		if (Input.GetKey(KeyCode.RightArrow))
+			transform.Translate(Vector3.right * predkosc);
+
+		float fov = Camera.main.fieldOfView;
+		fov += -Input.GetAxis("Mouse ScrollWheel") * predkoscZoom;
+		fov = Mathf.Clamp(fov, 0.1f, 179.9f);
+		Camera.main.fieldOfView = fov;
+	}
+}
---
+++ Assets/Skrypty/Kontroler.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Kontroler : MonoBehaviour {
+	//http://answers.unity3d.com/questions/359754/how-can-i-detect-touch-on-anroid-or-iphone.html
+
+	[SerializeField] bool wykrywanieMyszki = true;
+	[SerializeField] bool wykrywanieDotyku = false;
+
+	// Update is called once per frame
+	void Update(){
+		if(wykrywanieMyszki)
+			obslugaMyszki ();	
+		if(wykrywanieDotyku)
+			obslugaDotyku ();
+	}
+
+	void obslugaMyszki() {
+		if(Input.GetMouseButtonDown(0))
+		{    
+			bool trafionoObiekt = false;
+			GameObject obiekt;
+			Ray promien = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit trafienie = new RaycastHit();
+			trafionoObiekt = Physics.Raycast (promien, out trafienie);
+			if(trafionoObiekt)
+			{
+				obiekt = trafienie.transform.gameObject;
+				Debug.Log("Kliknieto : "+obiekt.name);
+				obslugaTrafionegoObiektu (obiekt);
+			}
+		}
+	}
+	void obslugaDotyku() {
+		if(Input.touchCount > 0) // == 1
+		{    
+			bool trafionoObiekt = false;
+			GameObject obiekt;
+			// touch on screen
+			if(Input.GetTouch(0).phase == TouchPhase.Began)
+			{
+				Ray promien = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+				RaycastHit trafienie = new RaycastHit();
+				trafionoObiekt = Physics.Raycast (promien, out trafienie);
+				if(trafionoObiekt)
+				{
+					obiekt = trafienie.transform.gameObject;
+					Debug.Log("Dotknieto : "+obiekt.name);
+					obslugaTrafionegoObiektu (obiekt);
+				}
+
+			}
+
+
+			// release touch/dragging
+			/*if((Input.GetTouch(0).phase == TouchPhase.Ended || Input.GetTouch(0).phase == TouchPhase.Canceled) && go != null)
+			{
+				moving = false;
+				Debug.Log("Touch Released from : "+go.name);
+			}*/
+		}
+	}
+
+	// metoda do testow
+	void obslugaTrafionegoObiektu(GameObject obiekt) {
+
+		if (obiekt.tag == "Pole") {
+			GameObject.Find ("Jednostka").GetComponent<RuchJednostki> ().przesunNaPozycje (obiekt.transform.position);
+		} 
+	}
+}
